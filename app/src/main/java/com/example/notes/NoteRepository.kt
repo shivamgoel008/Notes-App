a70f73073a43339a35b2c00f4066bc8dfb314549
Shivam Goel <shivamgoel150@gmail.com>
2021-03-02 23:35:53 +0530

    initial commit

---
+++ app/src/main/java/com/example/notes/NoteRepository.kt
@@ -0,0 +1,16 @@
+package com.example.notes
+
+import androidx.lifecycle.LiveData
+
+class NoteRepository (private val notesDao: NotesDao){
+
+    val allNotes : LiveData<List<Note>> = notesDao.getAllNotes()
+
+    suspend fun insert(note: Note){
+        notesDao.insert(note)
+    }
+
+    suspend fun delete(note: Note){
+        notesDao.delete(note)
+    }
+}
